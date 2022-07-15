fc98f43005e005817f68a161f6bbcc52a7dae535
ali <md.ali2001@hotmail.com>
2022-07-15 11:57:48 -0700

    repushed the last commit reason : css was not applying to signup and signin pages , renaming css files and a slight change in css worked

--- src/views/SignUp/index.js
+++ src/views/SignUp/index.js
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import { register } from "../../config/firebase";
 import { useNavigate } from "react-router-dom";
-import "./index.css";
+import "./signupindex.css";
 import { hover } from "@testing-library/user-event/dist/hover";
 
 
