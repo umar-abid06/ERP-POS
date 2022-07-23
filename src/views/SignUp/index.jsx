3318e719ad6970275dd9b34308cce05381192af4
ali <md.ali2001@hotmail.com>
2022-07-23 13:23:39 -0700

    converted inline css (sign in+sign up pages ) to external css

--- src/views/SignUp/index.jsx
+++ src/views/SignUp/index.jsx
@@ -35,7 +35,7 @@ function Signup() {
     <div>
       <div className="signup">
         <div className="column">
-          <h1 style={{position:"absolute",left:"600px",top:"30px",fontSize:"50px"}}>Sign Up Please...</h1>
+          <h1 className="a">Sign Up Please...</h1>
           <input style={{position:"absolute",top:"141px"}}
             onChange={(e) => setFullname(e.target.value)}
             placeholder="Write fullname here"
@@ -61,15 +61,16 @@ function Signup() {
 
           <br />
 
-          <img 
+          <img className="b"
           alt="Person's image" 
           src={'https://clipart.com/thumbs.php?f=/697/batch_42/000697-0042-000446_tnb.png'} 
-          style={{ height: "220px",width:"410px", position:"absolute",top:"150px",left:"100px" }} 
+  
         /> 
 
 
-          <button onClick={onSignup} style={{width:"90px",height:"40px",position:"absolute",left:"745px",top:"449px",backgroundColor:"green"}}><p style={{fontsize:"700px",color:"white"}}><b>Register</b></p></button>
-          <p onClick={() => navigate("/login")} style={{fontSize:"40px",position:"absolute",top:"540px",backgroundColor:"yellow",left:"500px"}} 
+          <button onClick={onSignup} style={{width:"90px",height:"40px",position:"absolute",left:"745px",  top:"449px",
+  backgroundColor:"green"}}><p style={{fontsize:"700px",color:"white"}}><b>Register</b></p></button>
+          <p onClick={() => navigate("/login")}  className="d"
           >
             <b>Already registered? Login here!</b>
           </p>
